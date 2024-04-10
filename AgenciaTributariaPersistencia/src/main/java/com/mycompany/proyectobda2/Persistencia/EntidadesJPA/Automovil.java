9e004050df367265cd9e1757a6bb0d907c9dc04e
JesusVillanuev <jesus.villanueva235078@potros.itson.edu.mx>
2024-04-09 21:22:03 -0700

    Modificacion de prueba

--- AgenciaTributariaPersistencia/src/main/java/com/mycompany/proyectobda2/Persistencia/EntidadesJPA/Automovil.java
+++ AgenciaTributariaPersistencia/src/main/java/com/mycompany/proyectobda2/Persistencia/EntidadesJPA/Automovil.java
@@ -16,41 +16,24 @@ import javax.persistence.PrimaryKeyJoinColumn;
  *
  * @author uirtis
  */
-@Entity(name = "automoviles")
+@Entity
 @DiscriminatorValue("automovil")
 @PrimaryKeyJoinColumn(referencedColumnName = "id")
 public class Automovil extends Vehiculo implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    
 
     public Automovil() {
         super();
     }
 
     public Automovil(Long id, String serieVehiculo, String marca, String modelo, String linea, String color, EstadoVehiculo estadoVehiculo, Persona persona) {
-        super(serieVehiculo, marca, modelo, linea, color, estadoVehiculo, persona);
-        this.id = id;
+        super(id,serieVehiculo, marca, modelo, linea, color, estadoVehiculo, persona);
 
     }
 
     public Automovil(String serieVehiculo, String marca, String modelo, String linea, String color, EstadoVehiculo estadoVehiculo, Persona persona) {
         super(serieVehiculo, marca, modelo, linea, color, estadoVehiculo, persona);
     }
-    
-    @Override
-    public Long getId() {
-        return id;
-    }
-    
-    @Override
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "Automovil{" + "id=" + id + '}';
-    }
+ 
 }
